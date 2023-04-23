28856b35012f6ca6d0ccad41412db3915a37b894
raulariias <raularias24685@gmail.com>
2023-04-23 15:00:36 -0400

    Hero section lista

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,18 +2,20 @@ import illustration from '../assets/illustration-working.svg'
 
 const HeroSection = () => {
   return (
-    <div className='px-[24px] overflow-x-hidden mb-[150px]'>
-      <div className='flex flex-col gap-[50px]'>
-        <img src={illustration} alt="Ilustracion de persona trabajando en una laptop" className='min-w-[500px] w-[733px]' />
-        <div className='flex flex-col gap-[30px] items-center'>
-          <h1 className='font-bold text-4xl text-center'>More than just shorter links</h1>
-          <p className='text-center text-[18px] text-gray-400'>Build your brand's recognition and get detailed insights on how your links are performing.</p>
-          <button className='w-[200px] h-[55px] bg-primary text-[18px] font-bold text-white rounded-[50px]'>
+    <section className='px-[24px] overflow-x-hidden mb-[150px] lg:px-0 lg:mt-[75px]'>
+      <div className='flex flex-col gap-[50px] lg:w-[75%] lg:mx-auto lg:flex-row-reverse lg:gap-[80px]'>
+        <div className='lg:box-border lg:w-[40%]'>
+          <img src={illustration} alt="Ilustracion de persona trabajando en una laptop" className='min-w-[500px] lg:min-w-[860px]' />
+        </div>
+        <div className='flex flex-col gap-[30px] items-center lg:w-[60%] lg:items-start lg:justify-center'>
+          <h1 className='font-bold text-4xl text-center lg:text-7xl lg:text-start'>More than just shorter links</h1>
+          <p className='text-center text-[18px] text-gray-400 lg:text-start lg:w-[80%] lg:text-[22px]'>Build your brand&apos;s recognition and get detailed insights on how your links are performing.</p>
+          <button className='w-[200px] h-[55px] bg-primary text-[18px] font-bold text-white rounded-[50px] lg:text-[22px] lg:w-[220px] lg:h-[60px]'>
             Get Started
           </button>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
